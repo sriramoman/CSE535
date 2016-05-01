9f083e01aee3ac63368548b1cf69a34ec7afcaca
sriramoman <sriramoman@gmail.com>
2016-05-01 16:27:53 -0700

    Notification

--- McProject/app/src/main/java/com/example/dhiraj/mcproject/PlaybackActivity.java
+++ McProject/app/src/main/java/com/example/dhiraj/mcproject/PlaybackActivity.java
@@ -3,6 +3,7 @@ package com.example.dhiraj.mcproject;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -477,6 +478,9 @@ public class PlaybackActivity extends AppCompatActivity implements OnChartValueS
     }
 
     private void stopPlayer(){
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager nMgr = (NotificationManager) getApplicationContext().getSystemService(ns);
+        nMgr.cancel(123);
         mediaPlayer.stop();
         mediaPlayer.reset();
         myHandler.removeCallbacksAndMessages(null);
